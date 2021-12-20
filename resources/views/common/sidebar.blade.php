adcc259ced82204deebb255d26dc4ff14319bd4e
FatinSyahidah <fatinsyahidah75@gmail.com>
2021-12-20 16:30:07 +0800

    changes

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -84,5 +84,14 @@
                     <span>Meeting</span>
                 </a>
             </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
             
         </ul>
