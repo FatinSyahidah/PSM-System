3431e3aa67082716e285d8bfd91295fd56e3c908
NadiaSauki <CB19045@student.ump.edu.my>
2021-12-20 14:59:05 +0800

    Ch@nges

--- resources/views/Manage Proposal/AddReq.blade.php
+++ resources/views/Manage Proposal/AddReq.blade.php
@@ -3,7 +3,7 @@
 <body>
 
 <form action="welcome_get.php" method="get">
-Name: <input type="text" name="name"><br>
+Name : <input type="text" name="name"><br>
 E-mail : <input type="text" name="email"><br>
 <input type="submit">
 </form>
