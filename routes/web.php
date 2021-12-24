932e5662dff50a8f444ecdce292e08410abc890c
NadiaSauki <CB19045@student.ump.edu.my>
2021-12-24 23:30:33 +0800

    changes

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/proposal', function () {
-    return view('StdMainPg');
-});
