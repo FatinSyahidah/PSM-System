5752d2d2f7381653a4e64bffec38cded7e7a75ec
puteridxyu <puteridxyu@gmail.com>
2022-01-13 07:49:52 +0800

    ..
    ..

--- resources/views/Manage Title/StatusTitle.blade.php
+++ resources/views/Manage Title/StatusTitle.blade.php
@@ -21,7 +21,7 @@
                 <div class="container-fluid">
 
                 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                    <h1 class="h3 mb-0 text-gray-800">Booking Title</h1>
+                    <h1 class="h3 mb-0 text-gray-800">Title Status</h1>
                    
                 </div>
 
