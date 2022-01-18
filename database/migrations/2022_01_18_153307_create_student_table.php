d6b0cb344ab24d84a1bd5d9880b8e5547995f53a
Nathania98 <84238217+Nathania98@users.noreply.github.com>
2022-01-19 00:30:03 +0800

    changes Thania

---
+++ database/migrations/2022_01_18_153307_create_student_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('stud_name');
+            $table->string('stud_matricID');
+            $table->string('stud_course');
+            $table->string('stud_year');
+            $table->string('stud_hpNum');
+            $table->string('stud_email');
+            $table->string('stud_add');
+            $table->string('stud_advisor');
+            $table->string('stud_psmTitle');
+            $table->string('stud_psmPhase');
+            $table->string('stud_profilePic');
+            $table->timestamps();
+
+
+            
+            
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+}
---
+++ database/migrations/2022_01_18_162023_create_lecturer_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLecturerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lecturer', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('lect_name');
+            $table->string('lect_staffID');
+            $table->string('lect_field');
+            $table->string('lect_officeNum');
+            $table->string('lect_hpNum');
+            $table->string('lect_email');
+            $table->string('lect_add');
+            $table->string('lect_expertise');
+            $table->string('lect_profilePic');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lecturer');
+    }
+}
