d4d6e7a5bd1633b65578478d8b103f97a48fe371
Manggaleswary <manggaleswarydiyadas@gmail.com>
2022-01-27 09:50:18 +0800

    ....

--- app/Http/Controllers/SupervisorController.php
+++ app/Http/Controllers/SupervisorController.php
@@ -3,82 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\InventoryModel\_book_s_v;
+use App\Models\InventoryModel\_stud_req;
+use Illuminate\Support\Facades\Auth;
 
 class SupervisorController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+
+    //View Expertise of Supervisor
+    public function viewExpertise()
     {
-        //
+        $data = DB::table('BookSV')->select('sv_id')->get();
+        return view('Manage Supervisor.SVbooking')->with('data',$data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    //Book the Supervisor
+    public function bookSV()
     {
-        //
+        $data = DB::table('BookSV')->select('sv_id')->get();
+        return view('Manage Supervisor.SVbooking')->with('data',$data);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    //View the booking status
+    public function viewBookingStaus()
     {
-        //
+        $data = DB::table('BookSV')->select('sv_id')->get();
+        return view('Manage Supervisor.SVbookingStaus')->with('data',$data);
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-}
