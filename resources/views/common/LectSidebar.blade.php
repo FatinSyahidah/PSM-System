f32083be6499f1179a65960cdc4f35dfe74455e8
NorhanizaSyafrizal <niza.ija99@gmail.com>
2022-01-27 08:36:43 +0800

    changes

--- resources/views/common/LectSidebar.blade.php
+++ resources/views/common/LectSidebar.blade.php
@@ -103,7 +103,7 @@
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         
-                        <a class="collapse-item" href="LogbookLecturer">Logbook</a>
+                        <a class="collapse-item" href="LectIndex">Logbook</a>
                         
                     </div>
                 </div>
